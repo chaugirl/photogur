d2ec38bbb6f0e9e9e6d2b79e6757bea10e7e769b
chaugirl <chau.marialuisa@gmail.com>
2014-11-04 16:06:10 -0500

    Create route and controller for http://localhost:3000

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,5 @@
+class PicturesController < ApplicationController  ## ApplicationController is a class given to us by rails
+	def index
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pictures' => 'pictures#index' ## create a new webpage at http://localhost:3000
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
